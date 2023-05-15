75ddca4600b7f5c0551b77b212b9ccfd25bca020
NoboruOtao <noboruotao@gmail.com>
2023-05-15 14:01:29 -0300

    seeder para todas as tabelas.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,25 +13,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
-// 
-        // // \Database\Factories\AcervoFactory::definition($num_editoras = 10, $num_autores = 20, $num_acervos = 50);
-// 
-        // \Database\Factories\PessoaCursoFactory::definition($num_pessoas = 100);
-// 
-        // \Database\Factories\AtividadeExtracurricularFactory::definition();
-// 
-        // \Database\Factories\DisciplinaFactory::definition();
-// 
-        // \Database\Factories\AreaDeConhecimentoFactory::definition();
-// 
-        // \Database\Factories\ClasseFactory::definition();
-// 
-        // \Database\Factories\NotaFactory::definition();
-// 
-        // \Database\Factories\PermissionAndRoleFactory::definition();
-// 
-        // \Database\Factories\EmprestimoFactory::definition();
+        \Illuminate\Support\Facades\Artisan::call('migrate:fresh');
+
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        \Database\Factories\AcervoFactory::definition($num_editoras = 100, $num_autores = 200, $num_acervos = 500);
+
+        \Database\Factories\PessoaCursoFactory::definition($num_pessoas = 500);
+
+        \Database\Factories\AtividadeExtracurricularFactory::definition();
+
+        \Database\Factories\DisciplinaFactory::definition();
+
+        \Database\Factories\AreaDeConhecimentoFactory::definition();
+
+        \Database\Factories\ClasseFactory::definition();
+
+        \Database\Factories\NotaFactory::definition();
+
+        \Database\Factories\PermissionAndRoleFactory::definition();
+
+        \Database\Factories\EmprestimoFactory::definition();
 
         \Database\Factories\SugestaoFactory::definition();
     }
